83118dae9fca60e1ffcf2944211db854f4802ad8
Zhou <susi.zhou22@gmail.com>
2023-08-14 12:09:52 +0200

    DbContext - Inyeccion de dependencias

---
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    // Creamos la clase y ponemos : DbContext para que herede de esa clase
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        // Aplicamos injeccion de dependencia. Creamos constructor poniendo ctor en el teclado y añadimos : base(options) porque hereda del padre
+        }
+        public DbSet<Villa> Villas { get; set; }
+        // Utilizamos metodo DbSet<> para crear una tabla en la Db cogiendo el modelo Villa y llamandolo Villas en la Db
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Nombre = "Villa Real",
+                    Detalle = "Detalle de la Villa",
+                    ImageUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 50,
+                    Tarifa = 200,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now,
+                },
+                new Villa()
+                {
+                    Id = 2,
+                    Nombre = "Villa Real",
+                    Detalle = "Detalle de la Villa",
+                    ImageUrl = "",
+                    Ocupantes = 2,
+                    MetrosCuadrados = 30,
+                    Tarifa = 650,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now,
+                }
+            );
+        // Utilizamos metodo override OnModelCreating que ya existe en la clase DbContext para cambiar caracteristicas
+        }
+    }
+}
--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,11 +1,20 @@
+using MagicVilla_API.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+// Agregamos nuevo servicio que va a enlazar la clase DbContext con la cadena de conexion y con SqlServer
+});
 
 var app = builder.Build();
 
